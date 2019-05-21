8e22165ce35a4e74bc6bccff1cc33fa277e9a050
glywk <glywk.contact@gmail.com>
2019-05-22 00:18:40 +0200

    Update properties_tree example header

--- example/properties_tree/properties_tree.cc
+++ example/properties_tree/properties_tree.cc
@@ -1,12 +1,10 @@
-// ----------------------------------------------------------------------------
-// Copyright (C) 2002-2006 Marcin Kalicinski
+//=============================================================================
+// Copyright (c) 2015-2018 glywk
+// https://github.com/glywk
 //
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-//
-// For more information, see www.boost.org
-// ----------------------------------------------------------------------------
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//=============================================================================
 #include <boost/foreach.hpp>
 #include <boost/property_tree/properties_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
